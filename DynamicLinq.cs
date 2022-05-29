0555a1276c288ca4e44a1eded9f18400c1501aaa
Valentin Lipatov <56578070+ValentinLipatov@users.noreply.github.com>
2022-05-29 15:25:24 +0300

    Add files via upload

---
+++ DynamicLinq.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Dynamic.Core;
+using System.Linq.Dynamic.Core.Parser;
+using System.Linq.Expressions;
+
+namespace DynamicLinq
+{
+    public static class DynamicLinq
+    {
+        static DynamicLinq()
+        {
+            _parsingConfig = new ParsingConfig();
+        }
+
+        private static ParsingConfig _parsingConfig;
+
+        public static TOut Invoke<TIn, TOut>(string expression, string parameterName, TIn parameter)
+        {
+            return (TOut)Invoke(typeof(TIn), typeof(TOut), expression, parameterName, parameter);
+        }
+
+        public static object Invoke(Type typeIn, Type typeOut, string expression, string parameterName, object parameter)
+        {
+            var expressionParameter = Expression.Parameter(typeIn, parameterName);
+            var expressionParser = new ExpressionParser(new[] { expressionParameter }, expression, new object[] { }, _parsingConfig);
+            var body = expressionParser.Parse(typeOut);
+            return Expression.Lambda(body, expressionParameter).Compile().DynamicInvoke(parameter);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DynamicLinq
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(DynamicLinq.Invoke<string, bool>("a.Contains(\"Hello\")", "a", "Hello world"));
+            Console.ReadKey();
+        }
+    }
+}
